a817f86f6a9d45201a699b7f3e564867d60b5bde
Entrenched-MMC <101645632+Entrenched-MMC@users.noreply.github.com>
2022-04-06 14:24:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+function display(id, btn) {
+  var dropper = document.getElementById(id);
+  var displaySetting = id.style.display;
+  var btn = document.getElementById(btn);
+  
+  if(displaySetting == 'block') {
+    var displaySetting = 'none';
+  }
+  if(displaySetting == 'none') {
+    var displaySetting = 'block';
+  }
+}
