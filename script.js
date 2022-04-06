dd7807bb1c3e03ebb78e60dc197fce1277bc42b6
Entrenched-MMC <101645632+Entrenched-MMC@users.noreply.github.com>
2022-04-06 16:29:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function display(id, btn) {
-  var dropper = document.getElementById(id);
-  var displaySetting = dropper.style.display;
-  var dropper-btn = document.getElementById(btn);
-  
-  if(displaySetting == 'block') {
-    var displaySetting = 'none';
-    var dropper-btn.innerHTML = 'Hide Dropdown';
-  }
-  else {
-    var displaySetting = 'block';
-    var dropper-btn.innerHTML = 'Other Links/Sub-Pages';
-  }
-}
+
